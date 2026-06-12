fdce362329286f029fc6cb997065e00c9c03db2c
LamThanh2k04 <ngolam2k04@gmail.com>
2026-06-12 20:30:13 +0700

    chore(app): add prismaModule to appModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { configModule } from './config/config.module';
+import { PrismaModule } from './common/prisma/prisma.module';
 @Module({
-  imports: [configModule],
+  imports: [configModule,PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
