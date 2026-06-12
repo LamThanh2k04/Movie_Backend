d471192cb66597311228a1cc0b3d3c481f4857ac
LamThanh2k04 <ngolam2k04@gmail.com>
2026-06-12 20:41:48 +0700

    chore(main): register exception at main

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AllExceptionFilter } from './common/filter/allException.filters';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalFilters(new AllExceptionFilter());
   await app.listen(process.env.PORT ?? 3000);
   console.log(`Server được chạy trên http://localhost:${process.env.PORT ?? 3000}`)
 }
