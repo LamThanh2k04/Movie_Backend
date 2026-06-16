b1e3d251ef822ce1a762fe6150b62b8234a5ec79
LamThanh2k04 <ngolam2k04@gmail.com>
2026-06-16 20:56:19 +0700

    chore(app): register authModule in appModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,9 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { configModule } from './config/config.module';
 import { PrismaModule } from './common/prisma/prisma.module';
+import { AuthModule } from './modules/auth/auth.module';
 @Module({
-  imports: [configModule,PrismaModule],
+  imports: [configModule,PrismaModule,AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
