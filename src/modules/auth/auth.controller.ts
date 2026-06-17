a333bb8d55a3cf584a80ab14d5a41c0bec874760
LamThanh2k04 <ngolam2k04@gmail.com>
2026-06-17 18:39:00 +0700

    feat(auth): implement user login

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -4,15 +4,22 @@ import { FileInterceptor } from '@nestjs/platform-express';
 import { RegisterDto } from './dto/register.dto';
 import { UploadedFileType } from 'src/common/types/uploadedFile.type';
 import { successResponse } from 'src/common/helper/response.helper';
+import { LoginDto } from './dto/login.dto';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(private readonly authService: AuthService) { }
 
   @Post('register')
   @UseInterceptors(FileInterceptor('avatar'))
-  async register (@Body() registerDto : RegisterDto, @UploadedFile() file : UploadedFileType) {
-    const data = await this.authService.register(registerDto,file)
-    return successResponse(data,'Đăng kí thành công', 201)
+  async register(@Body() registerDto: RegisterDto, @UploadedFile() file: UploadedFileType) {
+    const data = await this.authService.register(registerDto, file)
+    return successResponse(data, 'Đăng kí thành công', 201)
+  }
+
+  @Post('login')
+  async login(@Body() loginDto: LoginDto) {
+    const data = await this.authService.login(loginDto)
+    return successResponse(data, 'Đăng nhập thành công', 200)
   }
 }
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,15 +1,18 @@
-import { ConflictException, Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, UnauthorizedException } from '@nestjs/common';
 import { PrismaService } from 'src/common/prisma/prisma.service';
 import { RegisterDto } from './dto/register.dto';
 import * as bcrypt from 'bcrypt'
 import { UploadedFileType } from 'src/common/types/uploadedFile.type';
 import { CloudinaryService } from 'src/common/cloudinary/cloudinary.service';
+import { LoginDto } from './dto/login.dto';
+import { JwtService } from '@nestjs/jwt';
 @Injectable()
 export class AuthService {
     constructor
         (
             private prisma: PrismaService,
-            private cloudinary: CloudinaryService
+            private cloudinary: CloudinaryService,
+            private jwt: JwtService
         ) { }
 
     async register(registerDto: RegisterDto, file: UploadedFileType) {
@@ -36,5 +39,32 @@ export class AuthService {
             }
         })
         return user
+    }
+
+    async login(loginDto: LoginDto) {
+        const { email, password } = loginDto
+
+        const user = await this.prisma.user.findUnique({
+            where: { email }
+        })
+        if (!user) {
+            throw new UnauthorizedException("Email hoặc mật khẩu không đúng")
+        }
+
+        const isMatchedPassword = await bcrypt.compare(password, user.password)
+        if (!isMatchedPassword) {
+            throw new UnauthorizedException("Email hoặc mật khẩu không đúng")
+        }
+
+        const payload = {
+            sub: user.id,
+            role: user.role
+        }
+        const { password: _, ...safeUser } = user;
+        const token = await this.jwt.signAsync(payload)
+        return { 
+            accessToken: token,
+            user: safeUser
+         }
     }
 }
