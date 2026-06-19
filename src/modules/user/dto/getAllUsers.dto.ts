1b5be17481f58fe0f6ebdf273f849903d4a8851e
LamThanh2k04 <ngolam2k04@gmail.com>
2026-06-19 17:13:34 +0700

    feat(user): add getAllUsers DTO

---
+++ src/modules/user/dto/getAllUsers.dto.ts
@@ -0,0 +1,15 @@
+import { Type } from "class-transformer";
+import { IsInt, IsOptional, IsString, Min } from "class-validator";
+
+
+export class GetAllUsersDto {
+    @IsOptional()
+    @IsString()
+    search?: string
+
+    @IsOptional()
+    @Type(() => Number)
+    @IsInt()
+    @Min(1, { message: "Page phải lớn hơn hoặc bằng 1" })
+    page: number = 1
+}
