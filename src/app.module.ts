1a0068ca9ddba28ae5a04603deffeb15affc8cff
LamThanh2k04 <ngolam2k04@gmail.com>
2026-06-22 10:48:59 +0700

    chore(appModule): register genreModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,8 +5,9 @@ import { configModule } from './config/config.module';
 import { PrismaModule } from './common/prisma/prisma.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { UserModule } from './modules/user/user.module';
+import { GenreModule } from './modules/genre/genre.module';
 @Module({
-  imports: [configModule,PrismaModule,AuthModule,UserModule],
+  imports: [configModule,PrismaModule,AuthModule,UserModule,GenreModule],
   controllers: [AppController],
   providers: [AppService],
 })
