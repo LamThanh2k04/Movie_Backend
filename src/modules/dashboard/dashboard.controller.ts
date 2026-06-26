af24ac17c144394810e7b7ecb6af2898c58bc3cd
LamThanh2k04 <ngolam2k04@gmail.com>
2026-06-26 21:09:17 +0700

    feat(dashboard): add controller dashboard overview, total favorite movie, favorite timeline

---
+++ src/modules/dashboard/dashboard.controller.ts
@@ -0,0 +1,33 @@
+import { Controller, Get, Query, UseGuards } from '@nestjs/common';
+import { DashboardService } from './dashboard.service';
+import { successResponse } from 'src/common/helper/response.helper';
+import { Roles } from '../auth/decorators/role.decorators';
+import { Role } from '@prisma/client';
+import { JwtAuthGuard } from '../auth/guards/jwtAuth.guard';
+import { RoleGuard } from '../auth/guards/role.guard';
+import { FavoriteChartDto } from './dto/favoriteChart.dto';
+
+@Controller('dashboard')
+export class DashboardController {
+  constructor(private readonly dashboardService: DashboardService) { }
+
+  @Get('GetOverView')
+  @Roles(Role.ADMIN)
+  @UseGuards(JwtAuthGuard, RoleGuard)
+  async getAllOverView() {
+    const data = await this.dashboardService.getAllOverView()
+    return successResponse(data, 'Lấy tổng quan hệ thống thành công', 200)
+  }
+
+  @Get('getMovieFavoriteUser')
+  @Roles(Role.ADMIN)
+  @UseGuards(JwtAuthGuard, RoleGuard)
+  async getMovieFavoriteUser() { 
+     const data = await this.dashboardService.getMovieFavoriteUserChart()
+    return successResponse(data, 'Lấy top 5 phim được yêu thích nhiều nhất thành công', 200)
+  }
+  async getFavoriteChart(@Query() favoriteChartDto : FavoriteChartDto) {
+    const data = await this.dashboardService.getFavoriteChart(favoriteChartDto)
+    return successResponse(data,'Lấy thống kê lượt thích theo năm thành công',200)
+   }
+}
